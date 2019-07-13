f14c9ccfb877f9fae489a497fba577a3599c7974
iga-c <k.igari1013@gmail.com>
2019-07-13 22:16:06 +0900

    :sparkles: 詳細取得とツイート内容変更と開催時のみランキングページクロールするように修正 (#8)

--- crawler.py
+++ crawler.py
@@ -1,5 +1,9 @@
 import time
+import re
 from selenium import webdriver
+from datetime import datetime
+from datetime import timedelta
+from dateutil.parser import parse
 
 
 class GoldenLeagueCrawler:
@@ -9,16 +13,43 @@ class GoldenLeagueCrawler:
         self.webdriver_path = webdriver_path
         self.binary_location = binary_location
         self.retry_count = retry_count
+        self.crawl_end = False
 
     def crawl(self):
         self.generate_selenium_driver()
         self.driver.get("https://p.eagate.573.jp/game/ddr/ddra20/")
         time.sleep(3)
         self.driver.add_cookie({'name': 'M573SSID', 'value': self.cookie_value})
-        self.gold_u_border, self.gold_l_border, self.time_text = self.border_crawl("https://p.eagate.573.jp/game/ddr/ddra20/p/ranking/index.html?league_id=3&class_id=3", self.retry_count)
-        self.silver_u_border, self.silver_l_border, self.time_text = self.border_crawl("https://p.eagate.573.jp/game/ddr/ddra20/p/ranking/index.html?league_id=3&class_id=2", self.retry_count)
-        self.bronze_u_border, self.bronze_l_border, self.time_text = self.border_crawl("https://p.eagate.573.jp/game/ddr/ddra20/p/ranking/index.html?league_id=3&class_id=1", self.retry_count)
+        if not self.detail_crawl(self.retry_count):
+            print("クロールに失敗しました。")
+            self.driver.close()
+            return
+
+        if self.start_date < self.now_datetime() <= self.end_date + timedelta(hours=1):
+            self.gold_u_border, self.gold_l_border, self.time_text = self.border_crawl(self.ranking_page_url(self.league_count, 3), self.retry_count)
+            self.silver_u_border, self.silver_l_border, self.time_text = self.border_crawl(self.ranking_page_url(self.league_count, 2), self.retry_count)
+            self.bronze_u_border, self.bronze_l_border, self.time_text = self.border_crawl(self.ranking_page_url(self.league_count, 1), self.retry_count)
+        
         self.driver.close()
+        self.crawl_end = True
+
+    def detail_crawl(self, retry_count):
+        url = self.ranking_page_url()
+        for _ in range(retry_count):
+            self.driver.get(url)
+            time.sleep(3)
+
+            result = self.driver.find_elements_by_id("tournament")
+            if len(result) == 0:
+                continue
+
+            self.league_count = int(re.sub(r"\D", "", result[0].text))
+            dates_str = self.driver.find_elements_by_id("dates")[0].text
+            self.start_date = parse(dates_str[5:21])
+            self.end_date = parse(dates_str[22:])
+            return True
+        
+        return False
 
     def border_crawl(self, url, retry_count):
         for i in range(retry_count):
@@ -47,7 +78,21 @@ class GoldenLeagueCrawler:
         return u_border, l_border, time_text
 
     def tweet_text(self):
-        return_text = self.time_text + " 時点のボーダー:\n\n"
+        if not self.crawl_end:
+            return "クロールが終了していません。"
+
+        return_text = ""
+        if self.start_date == self.now_datetime():
+            return "第%d回ゴールデンリーグ開始です。" % self.league_count
+        if self.now_datetime() == self.end_date:
+            return "第%d回ゴールデンリーグ終了です。現在集計中になります。" % self.league_count
+        if self.now_datetime() == self.end_date + timedelta(hours=1):
+            return_text = "第%d回ゴールデンリーグお疲れ様でした。\n最終結果\n\n" % self.league_count
+        if self.start_date < self.now_datetime() < self.end_date:
+            return_text = self.time_text + " 時点のボーダー:\n\n"
+        if self.now_datetime() < self.start_date or self.end_date + timedelta(hours=1) < self.now_datetime():
+            return ""
+        
         return_text += "ゴールドクラス:\n"
         return_text += "降格ボーダー: " + self.gold_l_border + "\n\n"
         return_text += "シルバークラス:\n"
@@ -78,3 +123,16 @@ class GoldenLeagueCrawler:
         options.add_argument("--homedir=/tmp")
 
         self.driver = webdriver.Chrome(self.webdriver_path, options=options)
+
+    def ranking_page_url(self, league_id=0, class_id=0):
+        url = "https://p.eagate.573.jp/game/ddr/ddra20/p/ranking/index.html"
+        if 0 < league_id and 0 < class_id:
+            url += "?league_id=%d&class_id=%d" % league_id, class_id
+        
+        return url
+
+    def now_datetime(self):
+        if not hasattr(self, "now_time"):
+            self.now_time = datetime(datetime.now().year, datetime.now().month, datetime.now().day, datetime.now().hour)
+
+        return self.now_time
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import twitter
 import yaml
+import sys
 from crawler import GoldenLeagueCrawler
 
 
@@ -7,7 +8,13 @@ if __name__ == "__main__":
     f = open("config.yml", "r+")
     data = yaml.safe_load(f)
     crawler = GoldenLeagueCrawler(data["cookie_value"], data["webdriver_path"], '' if data["binary_location"] is None else data["binary_location"], data["retry_count"])
-    crawler.crawl()
+    if not crawler.crawl():
+        print("クロールに失敗しました。")
+        sys.exit()
+
+    tweet_text = crawler.tweet_text()
+    if tweet_text == "":
+        sys.exit()
     
     auth = twitter.OAuth(consumer_key=data["consumer_key"],
                      consumer_secret=data["consumer_secret"],
